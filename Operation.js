059b377ac7397ccd5a246c21ca7f61ab3b72d7b8
Mukkara Mukesh <mukkara.m@tvsnext.com>
2023-09-14 16:39:29 +0530

    [Mukesh] Add : Implemented to Add new contact

--- Operation.js
+++ Operation.js
@@ -3,10 +3,12 @@ const AddressBook = require('./AddressBook');
 
 const addressBook = new AddressBook();
 
-const contact1 = new Contact("Harry", "Potter", "4A-Main Street", "Hydrabad", "Telengana", "675476", "9876543210", "Harry@example.com");
-const contact2 = new Contact("Steve", "Smith", "41A-Sardar Colony", "Pune", "Maharastra", "987898", "9878987698", "Steve@example.com");
+const contact1 = new Contact("Harry", "Potter", "4A-Main Street", "Hydrabad", "Telengana", "675476", "9876543210", "harry@example.com");
+const contact2 = new Contact("Steve", "Smith", "41A-Sardar Colony", "Pune", "Maharastra", "987898", "9878987698", "steve@example.com");
+const contact3 = new Contact("John", "Son", "xcdsvdsvsd", "ndsddvssjdjk", "hdhuiu", "667774", "9878873658", "fhgfd@example.com");
 
 addressBook.addContact(contact1);
 addressBook.addContact(contact2);
+addressBook.addContact(contact3);
 
 addressBook.displayContacts();
