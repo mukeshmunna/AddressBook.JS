f5a9b08b2a0d7656138ba516edfdc78bb1309e0a
Mukkara Mukesh <mukkara.m@tvsnext.com>
2023-09-14 18:25:08 +0530

    [Mukesh] Add : Implemented to find Duplicate contact change in add contact

--- Operation.js
+++ Operation.js
@@ -6,6 +6,8 @@ const addressBook = new AddressBook();
 const contact1 = new Contact("Harry", "Potter", "4A-Main Street", "Hydrabad", "Telengana", "675476", "9876543210", "harry@example.com");
 const contact2 = new Contact("Steve", "Smith", "41A-Sardar Colony", "Pune", "Maharastra", "987898", "9878987698", "steve@example.com");
 const contact3 = new Contact("John", "Son", "xcdsvdsvsd", "ndsddvssjdjk", "hdhuiu", "667774", "9878873658", "fhgfd@example.com");
+const contact4 = new Contact("John", "Son", "xcdsvdsvsd", "ndsddvssjdjk", "hdhuiu", "667774", "9878873658", "fhgfd@example.com");
+
 
 addressBook.addContact(contact1);
 addressBook.addContact(contact2);
@@ -45,3 +47,4 @@ const contactCount = addressBook.getContactCount();
 console.log(`Number of Contacts: ${contactCount}`);
 
 addressBook.addContact(contact4);
+
